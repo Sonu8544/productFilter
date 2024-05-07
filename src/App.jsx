f354e4a22cde8c2b50ecab787f6509db4080001c
Sonu Kumar <sksonu8544@gmail.com>
2024-05-07 10:22:14 +0530

    DEV product fetch and show on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-export default function App() {
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://dummyjson.com/products');
+      const result = await response.json();
+      setProducts(result.products);
+      console.log(result.products)
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      <h1>Product Titles</h1>
+      <ul>
+        {products.map(product => (
+          <div>
+            <li key={product.id}>{product.title}</li>
+            <img src={product.images[0]} alt="" />
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
