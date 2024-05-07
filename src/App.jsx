a652b0ff5d3013ad030c235b52bfbb9ca896b8c5
Sonu Kumar <sksonu8544@gmail.com>
2024-05-07 08:45:25 +0530

    intrigate tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import './App.css'
-
-function App() {
-
+export default function App() {
   return (
-    <>
-      <h1>Lorem ipsum dolor sit.</h1>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
